f787cb18484ab26c128a61188f6290e0b16c1f25
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-05-30 21:37:53 +0300

    added main colors as variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["selector", '[data-mode="dark"]'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +12,19 @@ module.exports = {
         sans: ["Poppins", "sans-serif"],
       },
       gridTemplateColumns: { "70/30": "70% 28%" },
+    },
+    colors: {
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+      secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+
+      button_primary: "rgb(var(--color-button-primary) / <alpha-value>)",
+      button_secondary: "rgb(var(--color-button-secondary) / <alpha-value>)",
+
+      bg_primary: "rgb(var(--color-bg-primary) / <alpha-value>)",
+      bg_secondary: "rgb(var(--color-bg-secondary) / <alpha-value>)",
+      tertiary: "rgb(var(--color-bg-tertiary) / <alpha-value>)",
+
+      accent: "rgb(var(--color-accent) / <alpha-value>)",
     },
   },
   plugins: [],
